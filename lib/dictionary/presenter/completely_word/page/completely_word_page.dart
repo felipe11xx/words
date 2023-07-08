d54f019b8fb33879a7ed98674ba5c8ad19195dda
striker2x2 <felipe11xx@hotmail.com>
2023-07-08 01:02:52 -0300

    test_tts

--- lib/dictionary/presenter/completely_word/page/completely_word_page.dart
+++ lib/dictionary/presenter/completely_word/page/completely_word_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../../../../shared/widgets/app_progress_indicator.dart';
-import '../cubit/completely_word_cubit.dart';
 import '../cubit/completely_word_state.dart';
+import '../cubit/completely_word_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:text_to_speech/text_to_speech.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../shared/widgets/app_progress_indicator.dart';
 
 class CompletelyWordPage extends StatefulWidget {
   const CompletelyWordPage({super.key});
@@ -13,16 +14,24 @@ class CompletelyWordPage extends StatefulWidget {
 }
 
 class _CompletelyWordPageState extends State<CompletelyWordPage> {
-
- @override
+  @override
   void initState() {
-   context.read<CompletelyWordCubit>().getCompletelyWord('Hello');
-   super.initState();
- }
+    context.read<CompletelyWordCubit>().getCompletelyWord('Hello');
+    super.initState();
+  }
+  TextToSpeech tts = TextToSpeech();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          leading: IconButton(
+        color: Colors.black,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.close),
+      )),
       body: _blocConsumer(),
     );
   }
@@ -33,16 +42,35 @@ class _CompletelyWordPageState extends State<CompletelyWordPage> {
         builder: (context, state) {
           if (state is CompleteWordLoadingState) {
             return Center(
-              child: AppProgressIndicator(color: Colors.green,),
+              child: AppProgressIndicator(
+                color: Colors.green,
+                width: 50.w,
+              ),
             );
           }
           if (state is CompleteWordSuccessState) {
-            return Container(color: Colors.blue,);
+            return Container(
+              color: Colors.blue,
+              child: _body(),
+            );
           }
           if (state is CompleteWordLoadingState) {
-            return Container(color: Colors.red,);
+            return Container(
+              color: Colors.red,
+            );
           }
-          return Container(color: Colors.black,);
+          return Container(
+            color: Colors.black,
+          );
         });
+  }
+
+  _body(){
+    return Column(
+      children: [
+        Container(),
+        IconButton(onPressed: (){tts.speak('Eu consigo Falar');}, icon:const Icon(Icons.play_arrow_outlined), color: Colors.black,iconSize: 100.w,)
+      ],
+    );
   }
 }
